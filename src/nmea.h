bdb4c3639870e0b5eb80fc4401f83c16ed1278cc
Jonathan Hahn <helo9@users.noreply.github.com>
2021-04-23 09:25:31 +0200

    add testing for nmea sentence builder

---
+++ src/nmea.h
@@ -0,0 +1,7 @@
+#include "stdint.h"
+
+/* checksum for "PFLO," string part */
+#define PFLO_CS_SEED 57
+
+void nmeaflow_buildmsg(char *buf, double val);
+uint8_t nmeaflow_checksum(const char *line, int start_id, int end_id, uint8_t cs_seed);
